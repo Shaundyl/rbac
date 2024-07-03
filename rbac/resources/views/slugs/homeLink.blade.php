9a7f242a4f632e03efc803ecde24a71e2d48225a
Shaundyl <163128967+Shaundyl@users.noreply.github.com>
2024-07-03 10:46:16 +0800

    Delete rbac directory

--- rbac/resources/views/slugs/homeLink.blade.php
+++
@@ -1,1 +0,0 @@
-<span><a href="{{ route('home') }}">Back to Main Page</a></span>
--- rbac/resources/views/slugs/logout.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="{{ route('logout') }}" class="d-inline-block">
-    @csrf
-    <button type="submit" class="btn btn-sm btn-danger" style="--bs-btn-padding-y: .10rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
-        Logout
-    </button>
-</form>
